ec5b03957b5efd1b128e1b84739424ce9bfe7fa1
VanDuy <voduy12345duyv@gmail.com>
2023-10-12 10:31:48 +0700

    no message

--- src/Utils/crypto.ts
+++
@@ -1,8 +0,0 @@
-import { createHash } from 'crypto'
-
-export function sha256(content: string) {
-  return createHash('sha256').update(content).digest('hex')
-}
-export function hashPassword(password: string) {
-  return sha256(password + process.env.PASSWORD_SECRET)
-}
